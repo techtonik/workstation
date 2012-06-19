258bb2de114e7a114e489dc06e14379ac2ffdfdb
anatoly techtonik <techtonik@gmail.com>
2012-06-19 13:50:33 +0300

    Download required cookbooks automatically from recipe.

--- chef/config-solo.rb
+++ chef/config-solo.rb
@@ -1,5 +1,5 @@
 root = File.absolute_path(File.dirname(__FILE__))
 
 json_attribs root + "/config-solo.json"
-cookbook_path root + "/site-cookbooks"
+cookbook_path [root+"/cookbooks", root+"/site-cookbooks"]
 
--- chef/site-cookbooks/workstation/recipes/default.rb
+++ chef/site-cookbooks/workstation/recipes/default.rb
@@ -2,7 +2,14 @@ Chef::Log.info('--- Install basic packages ---')
 package 'git'
 package 'mercurial'
 
+
 Chef::Log.info('--- Download more cookbooks ---')
 # because vim is not packaged as 'vim' on Fedora
+# calculate root path to the 3rd party cookbooks
+thisdir = File.absolute_path(File.dirname(__FILE__))
+thiscook = File.dirname(File.dirname(thisdir))
+cookroot = File.dirname(thiscook) + "/cookbooks"
 
-
+git cookroot+"/vim" do
+  repository "git://github.com/opscode-cookbooks/vim.git"
+end

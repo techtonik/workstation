e66dad2f5e5b20024743adaea4cd9fbc10ff1e70
anatoly techtonik <techtonik@gmail.com>
2012-06-19 11:44:59 +0300

    Workstation bootstrap example with Chef-Solo configuration.

---
+++ chef/config-solo.rb
@@ -0,0 +1,5 @@
+root = File.absolute_path(File.dirname(__FILE__))
+
+json_attribs root + "/config-solo.json"
+cookbook_path root + "/site-cookbooks"
+
---
+++ chef/site-cookbooks/workstation/recipes/default.rb
@@ -0,0 +1,8 @@
+# --- Install basic packages ---
+package 'git'
+package 'mercurial'
+
+# --- Download more cookbooks ---
+# because vim is not packaged as 'vim' on Fedora
+
+

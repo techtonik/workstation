842a656a4cffdc29ef61250aa604cba0e291d8ad
anatoly techtonik <techtonik@gmail.com>
2012-06-23 13:06:50 +0300

    Use Librarian-Chef to manage Cookbook dependencies

--- chef/site-cookbooks/workstation/recipes/default.rb
+++ chef/site-cookbooks/workstation/recipes/default.rb
@@ -2,6 +2,20 @@ Chef::Log.info('--- Install basic packages ---')
 package 'git'
 package 'mercurial'
 
+Chef::Log.info('--- Packages by external cookbooks ---')
+# external cookbooks are specified in Cheffile managed
+# by Librarian-Chef. At the end of tile there is a failed
+# attempt to manage them from recipe itself.
+
+include_recipe 'vim'
+
+
+
+=begin
+# ^^^ this is multiline Ruby comment 
+# below is a failed attempt to Download dependent cookbooks
+# when they are not found from recipe itself. Now it is made
+# by Librarian as an extra prepatation step.
 
 Chef::Log.info('--- Download more cookbooks ---')
 # because vim is not packaged as 'vim' on Fedora
@@ -24,5 +38,7 @@ end
 execute "stub" do
   action :nothing
   include_recipe 'vim'
+  # this gives
+  #   FATAL: NoMethodError: undefined method `include_recipe' for Chef::Resource::Execute
 end
-
+=end

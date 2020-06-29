e075d1fb2e18abb8f04abf1587854e8d330b3511
anatoly techtonik <techtonik@gmail.com>
2020-06-29 19:04:07 +0300

    ansible.sh: use yaml callback plugin for more readable output

--- ansible.sh
+++ ansible.sh
@@ -5,5 +5,7 @@ ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$ROOT/ansible"
 
 #ANSIBLE_DISPLAY_ARGS_TO_STDOUT=True
-ANSIBLE_LOG_PATH=ansible.log ansible-playbook -v -i "$DIR/hosts" "$DIR/playbook.yml" --ask-become-pass $@
+ANSIBLE_LOG_PATH=ansible.log \
+ANSIBLE_STDOUT_CALLBACK=yaml \
+	ansible-playbook -v -i "$DIR/hosts" "$DIR/playbook.yml" --ask-become-pass $@
 
